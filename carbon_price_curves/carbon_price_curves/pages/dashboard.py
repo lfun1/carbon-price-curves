4fbb3a026dd432ae1849d2fcabf1ae3c73de4d35
Lisa Fung <lisafung710@gmail.com>
2024-02-17 14:12:30 -0800

    Add graph of CDR sales and improve year slider

--- carbon_price_curves/carbon_price_curves/pages/dashboard.py
+++ carbon_price_curves/carbon_price_curves/pages/dashboard.py
@@ -3,6 +3,7 @@ from carbon_price_curves.templates import template
 
 import reflex as rx
 import plotly.express as px
+import pandas as pd
 
 @template(route="/dashboard", title="Dashboard")
 def dashboard() -> rx.Component:
@@ -12,32 +13,38 @@ def dashboard() -> rx.Component:
         The UI for the dashboard page.
     """
 
-    df = px.data.gapminder().query("country=='Canada'")
+    # df = px.data.gapminder().query("country=='Canada'")
+    df = pd.read_csv('assets/cdr_data_test.csv')
 
     fig = px.line(
         df,
-        x="year",
-        y="lifeExp",
-        labels={
-            "year": "Year",
-            "lifeExp": "Life Expectancy (years)",
-        },
-        title="Life expectancy in Canada",
+        x="CDR Purchases",
+        y="Total Sales",
+        title="Global total carbon dioxide removal sales 2020-2023",
     )
 
     graph : rx.Component = rx.chakra.vstack(
-        rx.chakra.text("Graph"),
+        rx.chakra.text("Global total carbon dioxide removal sales 2020-2023"),
         rx.plotly(data=fig, height="400px"),
     )
 
     year_slider : rx.Component = slider_year_on_change()
 
     return rx.chakra.vstack(
-        rx.chakra.heading("Carbon Credits Forecast", font_size="3em"),
-        year_slider,
-        graph,
+        rx.chakra.heading("Carbon Credits Forecast", font_size="3em", padding="25px",),
+        rx.box(
+            year_slider,
+            width="100%",
+            padding="25px",
+        ),
+        rx.box(
+            graph,
+            width="60%",
+        ),
+        width="100%",
     )
 
+# Year Slider
 class SliderYearState(rx.State):
     year: int = 2024
 
@@ -46,7 +53,7 @@ class SliderYearState(rx.State):
 
 def slider_year_on_change() -> rx.Component:
     return rx.vstack(
-        rx.heading("Year: ", SliderYearState.year),
+        rx.heading("Year: ", SliderYearState.year, align="center"),
         rx.slider(
             default_value = 2030,
             min=2024,
@@ -55,4 +62,5 @@ def slider_year_on_change() -> rx.Component:
             on_change=SliderYearState.set_year,
             width="100%",
         ),
+        width="100%",
     )
